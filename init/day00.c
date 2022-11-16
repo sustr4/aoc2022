f78e45aff81432cb032a3df21811b834fcc9c42d
Zdenek Sustr <git@sustr.net>
2022-11-16 11:46:19 +0100

    More stock calls (allocation, printout, reading)

--- init/day00.c
+++ init/day00.c
@@ -5,6 +5,17 @@
 #include<malloc.h>
 #include<limits.h>
 
+// Boundary definitions, set as required
+#define MAXX 76
+#define MAXY 26
+
+// Point structure definition
+typedef struct {
+	int x;
+	int y;
+	int z;
+} TPoint;
+
 // Comparator function example
 int comp(const void *a, const void *b)
 {
@@ -16,7 +27,19 @@ int comp(const void *a, const void *b)
 // Example for calling qsort()
 //qsort(array,count,sizeof(),comp);
 
+
+// Print a two-dimensional array
+void printMap (char **map) {
+	int x,y;
+	for(y=0; y<MAXY; y++) {
+		for(x=0; x<MAXX; x++) {
+			printf("%c", map[y][x]);
+		}
+		printf("\n");
+	}
+}
 // Full block character for maps █
+
 
 // Read input file line by line (e.g., into an array)
 int readInput() {
@@ -26,9 +49,24 @@ int readInput() {
         ssize_t read;
 
         input = fopen("input.txt", "r");
-        if (input == NULL) exit(1);
+        if (input == NULL) {
+		fprintf(stderr,"Failed to open input file\n");
+		exit(1); }
+
+	// Allocate one-dimensional array of strings
+	// char **inst=(char**)calloc(MAXX, sizeof(char*));
+
+	// Allocate a two-dimensional arrray of chars
+	// int x=0, y=0;
+        // char **map=calloc(MAXY,sizeof(char*));
+	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
 
         while ((read = getline(&line, &len, input)) != -1) {
+
+		// Read into map
+		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
+		// y++;
+	
 	
 	}
 
@@ -36,7 +74,11 @@ int readInput() {
         if (line)
         free(line);
 
+//	printMap(map);
+
 	return 0;
+//	return inst;
+//	return map;
 }
 
 int main(int argc, char *argv[]) {
