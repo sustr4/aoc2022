13f35ffac0cdc7244ccfab484d092961ebe5f161
Zdenek Sustr <git@sustr.net>
2022-12-01 06:14:05 +0100

    Unused stuff out

--- day01/day01.c
+++ day01/day01.c
@@ -5,42 +5,6 @@
 #include<malloc.h>
 #include<limits.h>
 
-// Boundary definitions, set as required
-#define MAXX 76
-#define MAXY 26
-
-// Point structure definition
-typedef struct {
-	int x;
-	int y;
-	int z;
-} TPoint;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-  const int *da = (const int *) a;
-  const int *db = (const int *) b;
-  return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
-// Print a two-dimensional array
-void printMap (char **map) {
-	int x,y;
-	for(y=0; y<MAXY; y++) {
-		for(x=0; x<MAXX; x++) {
-			printf("%c", map[y][x]);
-		}
-		printf("\n");
-	}
-}
-// Full block character for maps █
-
-
 // Read input file line by line (e.g., into an array)
 int readInput() {
         FILE * input;
@@ -48,28 +12,25 @@ int readInput() {
         size_t len = 0;
         ssize_t read;
 	int count = 0;
+	int sum=0;
+	int hold;
 
         input = fopen("input.txt", "r");
         if (input == NULL) {
 		fprintf(stderr,"Failed to open input file\n");
 		exit(1); }
 
-	// Allocate one-dimensional array of strings
-	// char **inst=(char**)calloc(MAXX, sizeof(char*));
-
-	// Allocate a two-dimensional arrray of chars
-	// int x=0, y=0;
-        // char **map=calloc(MAXY,sizeof(char*));
-	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
 
         while ((read = getline(&line, &len, input)) != -1) {
 
-		// Read into map
-		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
-		// y++;
-
-		// Copy to string
-		//asprintf(&(inst[count]), "%s", line);	
+		if(strlen(line)>1) {
+			sscanf(line,"%d", &hold);
+			sum+=hold;
+		}
+		else {
+			printf("%d\n", sum);
+			sum=0;
+		}
 
 		count++;
 	}
@@ -78,15 +39,11 @@ int readInput() {
         if (line)
         free(line);
 
-//	printMap(map);
 
 	return 0;
-//	return inst;
-//	return map;
 }
 
 int main(int argc, char *argv[]) {
-
 	readInput();
 
 	return 0;
