49120273a642d81f402cba2971efe392892b269e
Zdenek Sustr <git@sustr.net>
2022-12-14 07:34:06 +0100

    Day 14, both tasks done

--- day14/day14.c
+++ day14/day14.c
@@ -6,32 +6,13 @@
 #include<limits.h>
 
 // Boundary definitions, set as required
-#define MAXX 600 // 457--517
+#define MAXX 1000 // 457--517
 #define MAXY 170 // 13--161
-
-// Point structure definition
-typedef struct {
-	int x;
-	int y;
-	int z;
-} TPoint;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-  const int *da = (const int *) a;
-  const int *db = (const int *) b;
-  return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
 
 // Print a two-dimensional array
 void printMap (char **map) {
 	int x,y;
-	for(y=0; y<163; y++) {
+	for(y=0; y<166; y++) {
 		for(x=456; x<519; x++) {
 			switch(map[y][x]) {
 				case 0:
@@ -48,7 +29,6 @@ void printMap (char **map) {
 		printf("\n");
 	}
 }
-// Full block character for maps █
 
 int drawLine(char** map, int fx, int fy, int tx, int ty) {
 	int i;
@@ -91,7 +71,6 @@ int drop(char **map) {
 
 // Read input file line by line (e.g., into an array)
 char **readInput() {
-//int readInput() {
         FILE * input;
         char * line = NULL;
         size_t len = 0;
@@ -105,34 +84,12 @@ char **readInput() {
 		fprintf(stderr,"Failed to open input file\n");
 		exit(1); }
 
-	// Allocate one-dimensional array of strings
-	// char **inst=(char**)calloc(MAXX, sizeof(char*));
-	// TPoint *inst=(TPoint*)calloc(MAXX, sizeof(TPoint));
-
-	// Allocate a two-dimensional arrray of chars
-	// int x=0, y=0;
         char **map=calloc(MAXY,sizeof(char*));
 	for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
 
         while ((read = getline(&line, &len, input)) != -1) {
 
-		// Read into map
-		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
-		// y++;
-
-		// Copy to string
-		//asprintf(&(inst[count]), "%s", line);	
-
-		// Read into array
-		// sscanf(line,"%d,%d",
-		//	&(inst[count].x),
-		//	&(inst[count].y));
-
-		// Read tokens from single line
-		//char *token;
-
 		token = strtok(line, " -> ");
-		printf("%s\n", token);
 		sscanf(token, "%d,%d", &fx, &fy);
 		while( 1 ) {
 			if(!(token = strtok(NULL, " -> "))) break;
@@ -140,8 +97,6 @@ char **readInput() {
 			drawLine(map, fx, fy, tx, ty);
 			fx=tx; fy=ty;
 		}
-
-
 		count++;
 	}
 
@@ -155,15 +110,15 @@ char **readInput() {
 
 int main(int argc, char *argv[]) {
 
-
-//	TPoint array;
-//	int i=0;	
-//	array = readInput();
 	char **map = readInput();
 	int count = 0;
 
-	while(drop(map)) count++;
+	// Comment the following line out for Task 1:
+	drawLine(map, 0, 163, 999, 163);
+
+	while((!map[0][500])&&drop(map)) count++;
 	printMap(map);
+
 
 	printf("Count: %d\n", count);
 
