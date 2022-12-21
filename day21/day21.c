e4855e659461f7fa07ab8737e38fbd6a042155ab
Zdenek Sustr <git@sustr.net>
2022-12-21 09:48:42 +0100

    Code cleanup

--- day21/day21.c
+++ day21/day21.c
@@ -33,31 +33,6 @@ typedef struct {
 int root;
 int humn;
 
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-  const int *da = (const int *) a;
-  const int *db = (const int *) b;
-  return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
-// Print a two-dimensional array
-void printMap (char **map) {
-	int x,y;
-	for(y=0; y<MAXY; y++) {
-		for(x=0; x<MAXX; x++) {
-			printf("%c", map[y][x]);
-		}
-		printf("\n");
-	}
-}
-// Full block character for maps █
-
-
 // Read input file line by line (e.g., into an array)
 TFormula *readInput() {
 //int readInput() {
@@ -74,19 +49,8 @@ TFormula *readInput() {
 		fprintf(stderr,"Failed to open input file\n");
 		exit(1); }
 
-	// Allocate one-dimensional array of strings
-	// char **inst=(char**)calloc(MAXX, sizeof(char*));
-
-
 	TFormula *inst=(TFormula*)calloc(MAXY, sizeof(TFormula));
 	ids=(char**)calloc(MAXY, sizeof(char*));
-	
-
-
-	// Allocate a two-dimensional arrray of chars
-	// int x=0, y=0;
-        // char **map=calloc(MAXY,sizeof(char*));
-	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
 
         while ((read = getline(&line, &len, input)) != -1) {
 
@@ -107,28 +71,8 @@ TFormula *readInput() {
 			inst[id].o=line[11];
 //			printf("%04d: %04d %c %04d\n", id, inst[id].op[0], inst[id].o, inst[id].op[1]);
 		}
-
 		if(!strncmp(line,"root",4)) root=id;
 		if(!strncmp(line,"humn",4)) humn=id;
-
-		// Read into map
-		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
-		// y++;
-
-		// Copy to string
-		//asprintf(&(inst[count]), "%s", line);	
-
-		// Read into array
-		// sscanf(line,"%d,%d",
-		//	&(inst[count].x),
-		//	&(inst[count].y));
-
-		// Read tokens from single line
-		//char *token;
-		//token = strtok(line, ",");
-		//while( 1 ) {
-		//	if(!(token = strtok(NULL, ","))) break;
-		//}
 
 		count++;
 	}
@@ -137,10 +81,7 @@ TFormula *readInput() {
         if (line)
         free(line);
 
-//	printMap(map);
-
 	return inst;
-//	return map;
 }
 
 long long int calculate(TFormula *array, int form) {
@@ -175,12 +116,7 @@ long long int calculate(TFormula *array, int form) {
 int main(int argc, char *argv[]) {
 
 	TFormula *array;
-//	int i=0;	
 	array = readInput();
-
-//	for(i=0; array[i]; i++) {
-//		printf("%d\n", array[i]);
-//	}
 
 	long long int rop, lop;
 
@@ -203,7 +139,6 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 	}
-
 
 	return 0;
 }
